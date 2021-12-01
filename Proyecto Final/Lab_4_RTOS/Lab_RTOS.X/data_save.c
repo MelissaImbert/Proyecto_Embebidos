55ef535ce609db10cba1c58fc58f7157b1c3b608
Juliocab <Juliocab2000@outlook.es>
2021-11-30 22:15:32 -0300

    Compila jejeje

--- Proyecto Final/Lab_4_RTOS/Lab_RTOS.X/data_save.c
+++ Proyecto Final/Lab_4_RTOS/Lab_RTOS.X/data_save.c
@@ -5,39 +5,49 @@
 #include <stdint.h>
 #include "time.h"
 #include "framework/GPS.h"
+#include "platform/utils.h"
+#include "mcc_generated_files/rtcc.h"
 /* ************************************************************************** */
 /* ************************************************************************** */
 /* Section: Included Files                                                    */
 /* ************************************************************************** */
 /* ************************************************************************** */
-
+#define RTCC_ACTUALIZATION 600000
 /* This section lists the other files that are included in this file.
  */
 
 /* TODO:  Include other files here if needed. */
-static  GPSPosition_t GPSpos; 
+static GPSPosition_t GPSpos;
 static struct tm GPStime;
+static ut_tmrDelay_t timer = {.state = UT_TMR_DELAY_INIT};
 
-void data_GPS(void){
+void data_GPS(void) {
     static uint8_t tramaGPS[110];
-    if (xSemaphoreTake(c_semGPSIsReady, portMAX_DELAY)== pdTRUE){
-        if(SIM808_getNMEA(tramaGPS)){
-            if(SIM808_validateNMEAFrame(tramaGPS)){
+    if (xSemaphoreTake(c_semGPSIsReady, portMAX_DELAY) == pdTRUE) {
+        if (SIM808_getNMEA(tramaGPS)) {
+            if (SIM808_validateNMEAFrame(tramaGPS)) {
                 GPS_getPosition(&GPSpos, tramaGPS);
                 GPS_getUTC(&GPStime, tramaGPS);
-                //Ver cuando setear RTCC
+                if (UT_delayms(&timer, RTCC_ACTUALIZATION)) {
+                    // Si pasó el tiempo especificado, actualizo RTCC con los datos del gps
+                    RTCC_TimeSet(&GPStime);
+                }
                 xSemaphoreGive(c_semGPSIsReady);
             } else {
-                //Ver que hago cuando no recibo nada
+                RTCC_TimeGet(&GPStime);
+                GPSpos.latitude = 500;
+                GPSpos.longitude = 500;
             }
+        } else {
+            RTCC_TimeGet(&GPStime);
+            GPSpos.latitude = 500;
+            GPSpos.longitude = 500;            
         }
     }
 }
 
-void datalog(void){
-    uint8_t googlemaps[300];
-    GPS_generateGoogleMaps( googlemaps, GPSpos );
-    
+void datalog(void) {
+
 }
 /* ************************************************************************** */
 /* ************************************************************************** */
