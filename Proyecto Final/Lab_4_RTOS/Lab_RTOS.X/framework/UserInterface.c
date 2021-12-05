7709512dd6e26e537d18ab875759cb99122aeaeb
AgustinCampos <90872204+AgustinCampos@users.noreply.github.com>
2021-12-05 12:33:39 -0300

    Revert "Cambios en Interfaz"
    This reverts commit 4282d0390c0e4cb550b8a0509f2d650c8c6497be.

--- Proyecto Final/Lab_4_RTOS/Lab_RTOS.X/framework/UserInterface.c
+++ Proyecto Final/Lab_4_RTOS/Lab_RTOS.X/framework/UserInterface.c
@@ -38,7 +38,7 @@ void initInterface(void *p_param) {
         //xSemaphoreTake(xopenInterface, portMAX_DELAY);
         if (USB_isReady()) {
             if (USB_recieve() > 0) {
-                xTaskCreate(interface, "task3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
+                xTaskCreate(interface, "task3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+3, NULL );
                 vTaskDelete(NULL);
             }
         }
@@ -48,7 +48,6 @@ void initInterface(void *p_param) {
 
 void interface(void *p_param) {
     MENU_STATES state;
-    MENU_STATES2 umbral;
     for (;;) {
         xTaskCreate(_puedoEnviar, "task4", 100, NULL, 2, NULL);
         if (xSemaphoreTake(xpuedoEnviar, portMAX_DELAY) == pdTRUE) {
@@ -63,47 +62,13 @@ void interface(void *p_param) {
             state = atoi(USB_getRxBuffer()) - 1;
             switch (state) {
                 case DOWNLOAD:
-                    xTaskCreate(_puedoEnviar, "task4", 100, NULL, 2, NULL);
-                    if (xSemaphoreTake(xpuedoEnviar, portMAX_DELAY) == pdTRUE) {
-                        USB_sendString("\n\nLISTA DE DATOS DEL GPS\n");
-                    }
+                    //if (Download()) {
                     break;
                 case THRESHOLDS:
-                    xTaskCreate(_puedoEnviar, "task4", 100, NULL, 2, NULL);
-                    if (xSemaphoreTake(xpuedoEnviar, portMAX_DELAY) == pdTRUE) {
-                        USB_sendString("\n\nCONFIGURACIÓN DE UMBRALES - Ingrese el umbral que desea configurar\n"
-                                "1 - Movimiento brusco\n"
-                                "2 - Choque\n"
-                                );
-
-                    }
-                    xTaskCreate(puedoRecibir, "task4", 100, NULL, 2, NULL);
-                    if (xSemaphoreTake(xpuedoRecibir, portMAX_DELAY) == pdTRUE) {
-                        umbral = atoi(USB_getRxBuffer()) - 1;
-                        switch (umbral) {
-                            case BRUSCO:
-                                definirUmbral();
-                                //valor_umbral = definirUmbral();
-                                break;
-                            case CHOQUE:
-                                definirUmbral();
-                                //valor_umbral = definirUmbral();
-                                break;
-
-                        }
-                    }
-
+                    definirUmbral();
                     break;
                 case PERIOD:
-                    xTaskCreate(_puedoEnviar, "task4", 100, NULL, 2, NULL);
-                    if (xSemaphoreTake(xpuedoEnviar, portMAX_DELAY) == pdTRUE) {
-                        USB_sendString("\n\nFRECUENCIA DEL LOGEO DEL GPS - Ingrese el período que desee (en milisegundos)\n");
-
-                    }
-                    xTaskCreate(puedoRecibir, "task4", 100, NULL, 2, NULL);
-                    if (xSemaphoreTake(xpuedoRecibir, portMAX_DELAY) == pdTRUE) {
-                        //Periodo = atoi(USB_getRxBuffer()); (Perodo = parametro para el timer que controle al gps)
-                    }
+                    //if (Period()) {
                     break;
                 default:
                     break;
